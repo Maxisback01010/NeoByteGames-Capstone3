3c8f22f8b4d9e3121051d9a9a063960c8f294261
Max <marleyjodici@gmail.com>
2026-06-25 00:52:22 -0700

    Implemented Categories GET endpoints and connected service layer

--- capstone-starter-files (2)/capstone-starter-files/capstone-api-starter/src/main/java/org/yearup/controllers/CategoriesController.java
+++ capstone-starter-files (2)/capstone-starter-files/capstone-api-starter/src/main/java/org/yearup/controllers/CategoriesController.java
@@ -2,9 +2,7 @@ package org.yearup.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 import org.yearup.models.Category;
 import org.yearup.models.Product;
 import org.yearup.service.CategoryService;
@@ -16,6 +14,9 @@ import java.util.List;
 // add the annotation to make this controller the endpoint for the following url
     // http://localhost:8080/categories
 // add annotation to allow cross site origin requests
+@RestController
+@RequestMapping("/categories")
+@CrossOrigin
 public class CategoriesController
 {
     private CategoryService categoryService;
